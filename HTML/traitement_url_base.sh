3537151313cda5cd4554df079682bcdb3943a1bd
luisanb <luisanbatista@gmail.com>
2022-11-27 21:03:20 +0100

    html mises a jour

--- HTML/traitement_url_base.sh
+++ HTML/traitement_url_base.sh
@@ -14,15 +14,19 @@ fichier_tableau=$2 # le fichier HTML en sortie
 
 
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
-if [ $# - ne 2 ]
+if [[ $# -ne 2 ]]  # si le nombre d'arguments est différent de 2
 then
-	echo "ce programme demande un argument"
-		exit
+	echo "Ce programme demande un argument"
+	exit
 fi
 
+mot="trem" # à réfléchir
 
 # modifier la ligne suivante pour créer effectivement du HTML
 # echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
+
+echo $fichier_urls;
+basename=$(basename -s .txt $fichier_urls)
 
 echo "<html>
 	<head>
@@ -30,42 +34,54 @@ echo "<html>
 	</head>
 	<body>
 		<p>Tableau d'URLS en portugais</p>
+		    <table border=solid width=100%>" > $fichier_tableau
+echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
+echo "<br/>" >> $fichier_tableau
+echo "<table>" >> $fichier_tableau
+echo "<tr><th>LIGNE</th><th>CODE</th><th>URL</th><th>encodage</th></tr>" >> $fichier_tableau
 
-		    <table border=solid width=100%>
-		    	<tr>
-    	   		  <th>Nº</th>
-    	  		  <th>SITES $1</th>
-		 	  <th>LIENS</th>
-    			</tr> $fichier_tableau 
+# -d= délimiteur
+# f terminologie table field / champs
 
-	</body> 
-
-
-
-while read -r LINE ;
+lineno=1;  # line number
+while read -r URL;
 do
-	resultat=$(curl --head --silent $LINE | grep -i -P -o "charset" | cut -d " " -f 2 | head -n1)
-	if [[ $resultat -eq 200 ]]
-	then
-	echo "
-		<tr><td>1</td><td>"$resultat"</td><td>"$LINE"</td></tr>" >> $fichier_tableau
+	echo -e "\tURL : $URL";
 	
+	# il y a deux options de le faire
+	# code=$(curl --head --silent $URL | grep -i -P -o "^HTTP/" | cut -d " " -f 2 | tail -n 1) # on prendre la dernière ligne 
+	
+	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1) 
+	charset=$(curl -ILs $URL | egrep -o "charset=(\w|-)+" | cut -d= -f2 | tail -n 1) 
+	# -I - pour recuperer l'en-tete, -f2 pour recuperer la deuxieme colonne, -s - mode silencieux
+	echo -e "\tcode : $code";
+	
+	if [[ ! $chasert ]] # s'il n'existe pas
+	then
+		echo -e "\tencodage non détecté, on prendra UTF-8 par défaut."; 
+	else
+		echo -e "\tencodage : $charset";
 	fi
+	
+	if [[ $code -eq 200 ]]
+	then
+		aspiration=$(curl $URL > ../aspirations/$basename-$lineno.html)
+		dump=$(lynx -dump -nolist -assume_chasert=$chasert -display_charset=$charset $URL)
+		if [[ $chasert -ne "UTF-8" && -n "$dump" ]] # au cas où il n'y a pas cet encodage, on va le convertir
+		then 
+			dump=$(echo $dump | iconv -if $charset -t UTF-8//IGNORE)
+		fi
+	else
+		echo -e "\tcode différent de 200 utilisation d'un dump vide"
+		dump=""
+		chasert=""
+	fi	
+	
+	
+	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td></tr>" >> $fichier_tableau
+	echo -e "\t--------------------------------"
+	lineno=$((lineno+1));
+	echo "$dump" > ../dumps-text/$basename-$lineno
 done < $fichier_urls
-
-echo "
-
-	</table>
-   </body>
-</html>
-
-" >> $fichier_tableau
-
-
-#lineno=1;
-
-#while read -r line;
-#do
-#	echo "ligne $lineno: $line";
-#	lineno=$((lineno+1));
-#done < $fichier_urls
+echo "</table>" >> $fichier_tableau
+echo "</body></html>" >> $fichier_tableau
--- premier_script.sh
+++ premier_script.sh
@@ -1,1 +1,2 @@
 # traitement de l'année 2016, on crée on écrase le fichier sortie.txt
+
